287acbec65ed96162d79d50be94c13ab38d9977f
Lahssen K <>
2023-07-10 17:50:33 +0200

    replace src/sub/mod.rs with src/sub.rs

--- modularity/src/sub.rs
+++ modularity/src/sub.rs
@@ -1,5 +1,10 @@
+//! This module contains a private and a public sub modules.
+//! This is a simple module level acting as inner documentation.
+
 // make sub::private only visible to sibling modules
 mod private;
 // make sub::public visible as public
 pub mod public;
 // not adding sub::hidden on purpose to hide it from siblings
+
+// sub.rs replaces the old way: sub/mod.rs
