301860db0dd2be6c085794604e9862b03c8a3987
Lahssen K <>
2023-07-11 17:49:36 +0200

    trait objects

--- interfaces/src/main.rs
+++ interfaces/src/main.rs
@@ -5,6 +5,38 @@ fn main() {
     };
     john.print_age();
     john.describe();
+
+    let pets: Vec<Box<dyn Pet>> = vec![
+        Box::new(Cat),
+        Box::new(Dog {
+            name: String::from("Fido"),
+        }),
+    ];
+    for pet in pets {
+        println!("Hello {}!", pet.name());
+    }
+}
+
+trait Pet {
+    fn name(&self) -> String;
+}
+
+struct Dog {
+    name: String,
+}
+
+struct Cat;
+
+impl Pet for Dog {
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+}
+
+impl Pet for Cat {
+    fn name(&self) -> String {
+        String::from("The cat") // No name, cats won't respond to it anyway.
+    }
 }
 
 trait PrintAge {
