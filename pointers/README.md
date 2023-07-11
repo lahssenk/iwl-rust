ddf4e3b15c2cfc1bf2c6ae9fa178b6b09a5652b3
Lahssen K <>
2023-07-11 15:59:14 +0200

    pointers, references, threads and some smart pointers

--- pointers/README.md
+++ pointers/README.md
@@ -1,3 +1,31 @@
 # Pointers
 
 This chapter covers pointers, references lifetimes and the ownership model of rust.
+
+## References
+
+Rust presents an abstraction around pointers, called references. A reference provides
+the guarantee to not be nil at compile time, making it safer to share access to resources
+via pointers.
+
+Types of references:
+- immutable, using the `&` sign in front of a variable name
+- mutable, using the `&mut` keyword in front of a variable name
+
+The rust compiler ensures that `&mut` is exclusive (no other `&mut` or `&` can exist)
+and that there can be many `&` safely. (allowing for multiple concurrent readers or a unique writer)
+
+# Send and Sync traits
+
+Each basic type is defined in the language with some Send and Sync capability.
+A type is Send if the value can be sent from a thread to another safely.
+A type is Sync if a reference to the value can be sent from a thread to another safely.
+
+# Smart Pointers
+
+In rust, raw references are very powerful and form the foundation for its "fearless concurrency" paradigm. Despite being a power concept, some types dedicated to sharing data are included in the language to help with more complex use-cases.
+
+- Box: a generic pointer to some value that will be stored in the heap. Useful because every value needs to have a knows size at compile time, growable types therefor need to be stored on heap so that a static variable (header type) can be used as a handle to the underlying backing data
+- Rc (Reference Counter): useful for keeping track of references to a value when it is not known in advance which will be the last reference to stay alive and when to drop the value (free memory)
+- Arc (Atomic Reference Counter): slower but thread safe reference counter. It makes a type Send
+- Mutex: a wrapper around a type that provides locking primitives. It returns a lock guard around the value, that automatically unlocks the mutex when the guard goes out of scope (dropped)
--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -1,3 +1,76 @@
+use std::{rc::Rc, sync::Arc};
+
 fn main() {
-    println!("Hello, world!");
+    // create a value
+    let s1 = String::from("abc");
+    // move the value
+    let _s2 = s1;
+
+    // the following line would cause a compiler error because the ownership of the data store
+    // as s1 has been transfered/moved to s2 and can not be accessed via s1 anymore
+
+    //println!("s1: {s1}");
+
+    let mut val = 100;
+    val += 1;
+    assert_eq!(val, 101);
+
+    // create multiple read-only references
+    let ref1 = &val;
+    let ref2 = &val;
+
+    // println will automatically dereference, so it will print the value not the address
+    println!("val={val}, ref1={ref1}, ref2={ref2}");
+
+    // create a mutable reference
+    let mut_ref = &mut val;
+    // derefence it and increment the value
+    *mut_ref += 2;
+    assert_eq!(val, 103);
+    println!("val={val}");
+
+    // uncommenting the following line would cause a compiler error because &mut and & can not coexist
+    // println!("ref1={ref1}");
+
+    // smart pointers ------
+    let val = String::from("some very long string you do not want to copy over and over again");
+    println!("val: {val}");
+    let rc = Rc::new(val);
+    // cloning the value increments an internal counter, it does not clone the underlying data
+    let clone1 = rc.clone();
+    println!("clone1: {clone1}");
+    let clone2 = rc.clone();
+    println!("clone2: {clone2}");
+
+    // we now have 2 references to the same data
+    {
+        // the compiler can not know which branch will execute and therefor
+        // we use Rc to do the accounting and garbage collection (drop val when clone1 and clone2 have been dropped)
+        let choice = 1;
+        match choice {
+            1 => println!("chosen clone1: {clone1}"),
+            _ => println!("chose clone2: {clone2}"),
+        }
+    }
+
+    let val = String::from("some contents");
+    // let's wrap the value into an Arc, because we'll spawn threads
+    // and compiler does not know which reference should be dropped first
+    let arc = Arc::new(val);
+    let clone1 = arc.clone();
+    let clone2 = arc.clone();
+    // spawn a thread executing a closure
+    let thread1 = std::thread::spawn(move || {
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        println!("clone1: {clone1}");
+    });
+    // another thread
+    let thread2 = std::thread::spawn(move || {
+        std::thread::sleep(std::time::Duration::from_millis(50));
+        println!("clone2: {clone2}");
+    });
+
+    // wait for threads to return
+    thread1.join().expect("first thread has panicked");
+    thread2.join().expect("second thread has panicked");
 }
